08d0d2e29173ee36a6a19e8323f622236f00a21c
sundrecommunication-arch <sundrecommunication@gmail.com>
2026-06-20 09:03:53 +0100

    fix: mobile scroll and CSP Paystack stylesheet

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use((req, res, next) => {
   res.setHeader('Content-Security-Policy', [
     "default-src 'self'",
     "script-src 'self' 'unsafe-inline' https://js.paystack.co",
-    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://js.paystack.co https://checkout.paystack.com",
+    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.gstatic.com https://js.paystack.co https://checkout.paystack.com https://paystack.com",
     "font-src 'self' https://fonts.gstatic.com",
     "img-src 'self' data: https:",
     "connect-src 'self' https://api.anthropic.com https://*.supabase.co https://api.paystack.co https://api.ng.termii.com",
